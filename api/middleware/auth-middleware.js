6c805e442281635232c53d14d0855802800dcac3
Anthony Coman <acoman@yahoo.com>
2022-01-27 21:31:58 -0600

    wip

--- api/middleware/auth-middleware.js
+++ api/middleware/auth-middleware.js
@@ -9,24 +9,24 @@ const checkBodyExists = (req, res, next) => {
 }
 
 const checkUsernameExists = async (req, res, next) => {
-	try {
-		const user = await db('users').where('username', req.body.username)
-		if (user.length || user === req.body.username) {
-			if (req.originalUrl === '/api/auth/login') {
+	// try {
+	// 	const user = await db('users').where('username', req.body.username)
+	// 	if (user.length || user === req.body.username) {
+	// 		if (req.originalUrl === '/api/auth/login') {
 				next()
-			} else if (req.originalUrl === '/api/auth/register') {
-				next({ status: 401, message: 'username taken' })
-			} else {
-				next({ status: 401, message: 'Invalid credentials' })
-			}
-		} else {
-			if (req.originalUrl === '/api/auth/login') {
-				next({ status: 401, message: 'Invalid credentials' })
-			}
-		}
-	} catch (err) {
-		next(err)
-	}
+	// 		} else if (req.originalUrl === '/api/auth/register') {
+	// 			next({ status: 401, message: 'username taken' })
+	// 		} else {
+	// 			next({ status: 401, message: 'Invalid credentials' })
+	// 		}
+	// 	} else {
+	// 		if (req.originalUrl === '/api/auth/login') {
+	// 			next({ status: 401, message: 'Invalid credentials' })
+	// 		}
+	// 	}
+	// } catch (err) {
+	// 	next(err)
+	// }
 }
 
 module.exports = {
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,9 @@
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).not.toBe(false)
 })
+
